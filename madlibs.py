43d9918e400bdbc436f849d0ba91b23c2bc307a7
ransomv <rvarley2001@yahoo.com>
2015-09-17 10:25:30 -0700

    Committed after changes

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,11 @@
-hello
+name = input("Please enter a pet's name: ")
+print(name)
+madlib = name
+madlib += " ate a bunch of "
+madlib += input("Enter a food: ")
+madlib += " and got a case of "
+madlib += input("Enter and illness: ")
+madlib += ".  \nAfter several hours, {} had a full recovery." .format(name)
+
+
+print(madlib)
